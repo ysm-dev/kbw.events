412215ac8b9dafbf74808db9676ee870e9ce1675
ysm-dev <i@ysm.dev>
2024-08-05 03:48:13 +0900

    scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: Props) {
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
       </head>
       <body className="antialiased">
